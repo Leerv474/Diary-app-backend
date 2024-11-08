327063e6c7e4e574cadc03040ba93b5831e568bf
Leerv474 <roman.lee474@gmail.com>
2024-11-08 22:30:57 +0300

    tweaks

--- diary/src/main/java/com/leerv/diary/repositories/UserRepository.java
+++ diary/src/main/java/com/leerv/diary/repositories/UserRepository.java
@@ -2,9 +2,18 @@ package com.leerv.diary.repositories;
 
 import com.leerv.diary.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
+
+    @Query("""
+            select user
+            from User user
+            where user.accountLocked = true
+            """)
+    List<User> findAllLocked();
 }
